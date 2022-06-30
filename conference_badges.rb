e740f14a25ccbe4aaab54cda564a848cd42325cd
Devin Garcia <devingarcia5423@gmail.com>
2022-06-29 20:33:12 -0400

    commit tree 92b5a23dcdb2419df86b0e4f9cbd7258727d8cd4

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker name
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator arr
+  arr.map {|name| badge_maker(name)}
+end
+
+def assign_rooms arr
+  i = 0
+  arr.map do |name|
+    i += 1
+    "Hello, #{name}! You'll be assigned to room #{i}!"
+  end
+end
+
+def printer arr
+  batch_badge_creator(arr).map {|ind| puts ind }
+  assign_rooms(arr).map {|ind| puts ind }
+end
